f131bb923f7fb94e90bf57ea1b25c51ce4dce101
Sebastian Kleinschmidt <sebastiankleinschmidt@hotmail.com>
2020-06-12 19:01:29 +0200

    feat: implements unit-test for subtraction

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from app.calc import add
+from app.calc import add, subtract
 
 
 def CalcTests(TestCase):
@@ -8,3 +8,7 @@ def CalcTests(TestCase):
     def test_add_numbers(self):
         """ Test that two numbers are added together """
         self.assertEqual(add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        """ Test that two numbers are substracted correctly """
+        self.assertEqual(subtract(5, 7), -2)
